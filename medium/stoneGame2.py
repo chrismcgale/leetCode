86b10c2dc9f74ad7742164cffef17a8433ef8b30
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2023-05-25 21:24:07 -0400

    stoneGame2 10x speedup

--- medium/stoneGame2.py
+++ medium/stoneGame2.py
@@ -6,22 +6,31 @@ class Solution:
         def Alice(index: int, X: int) -> int:
             if index >= n:
                 return 0
-            
-            m = 0
-            for i in range(1, 2 * X + 1):
-                m = max(m, sum(piles[index : index + i]) + Bob(index + i, max(X, i)))
-        
-            return m
+    
+            return max(sum(piles[index : index + i]) + Bob(index + i, max(X, i)) for i in range(1, 2 * X + 1))
+
         
         @cache
         def Bob(index: int, X: int) -> int:
             if index >= n:
                 return 0
             
-            m = sys.maxsize
-            for i in range(1, 2 * X + 1):
-                m = min(m, Alice(index + i, max(X, i)))
+            return min(Alice(index + i, max(X, i)) for i in range(1, 2 * X + 1))
         
-            return m
         
         return Alice(0, 1)
+    
+    def fasterStoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        for i in range(n - 2, -1, -1):
+            piles[i] += piles[i + 1] 
+        
+        @cache
+        def dp(index: int, X: int) -> int:
+            if index + 2 * X >= n:
+                return piles[index]
+    
+            return piles[index] - min(dp(index + i, max(X, i)) for i in range(1, 2 * X + 1))
+
+        
+        return dp(0, 1)
