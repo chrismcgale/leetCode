40ec9ab8faa46afae1bd7a065521becb3b3dceb9
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2023-07-17 09:22:34 -0400

    addTwoNums2

---
+++ medium/addTwoNums2.py
@@ -0,0 +1,87 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        n1, n2 = 0, 0
+        
+        while l1:
+            n1 = n1 * 10 + l1.val
+            l1 = l1.next
+            
+        while l2:
+            n2 = n2 * 10 + l2.val
+            l2 = l2.next
+            
+        n = n1 + n2
+        head = ListNode()
+        curr = head
+        
+        for num in str(n):
+            curr.next = ListNode(num)
+            curr = curr.next
+            
+        return head.next
+    
+    def addTwoNumbersStack(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1 = []
+        s2 = []
+
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+
+        total_sum = 0
+        carry = 0
+        ans = ListNode()
+        while s1 or s2:
+            if s1:
+                total_sum += s1.pop()
+            if s2:
+                total_sum += s2.pop()
+
+            ans.val = total_sum % 10
+            carry = total_sum // 10
+            head = ListNode(carry)
+            head.next = ans
+            ans = head
+            total_sum = carry
+
+        return ans.next if carry == 0 else ans
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        temp = None
+        while head:
+            # Keep the next node
+            temp = head.next
+            # Reverse the link
+            head.next = prev
+            # Update the previous node and the current node.
+            prev = head
+            head = temp
+        return prev
+
+    def addTwoNumbersRev(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        r1 = self.reverseList(l1)
+        r2 = self.reverseList(l2)
+
+        total_sum = 0
+        carry = 0
+        ans = ListNode()
+        while r1 or r2:
+            if r1:
+                total_sum += r1.val
+                r1 = r1.next
+            if r2:
+                total_sum += r2.val
+                r2 = r2.next
+
+            ans.val = total_sum % 10
+            carry = total_sum // 10
+            head = ListNode(carry)
+            head.next = ans
+            ans = head
+            total_sum = carry
+
+        return ans.next if carry == 0 else ans
