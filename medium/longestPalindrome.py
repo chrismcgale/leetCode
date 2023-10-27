c576ad50be7a4212b439fab57568ecb1dd9babc8
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2023-10-27 11:10:41 -0400

    longest_pal

--- medium/longestPalindrome.py
+++ medium/longestPalindrome.py
@@ -47,4 +47,24 @@ class Solution:
                 end = i + m // 2
         
         return s[start : end + 1]
+    
+    def longestPalindromeTD(self, s: str) -> str:
+        n = len(s)
+        is_pal = [[False if j != i else True for j in range(n)] for i in range(n)]
+        m_sub = s[0]
+        
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                is_pal[i][i + 1] = True
+                m_sub = s[i: i + 2]
+        
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff         
+                if s[i] == s[j] and is_pal[i + 1][j - 1]:
+                    is_pal[i][j] = True
+                    m_sub = s[i:j + 1]
+                    
+        return m_sub
+                    
                 
